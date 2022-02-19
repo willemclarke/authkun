b226f417f1d0db36ac8d636911aca25eab5cafaa
willemclarke <willem.2@hotmail.com>
2022-02-19 13:24:06 +1000

    feat(app): add json script to connect to docker postgres via pgcli, create users table

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 4000;
+const PORT = 8080;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:8081' }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome sire');
+});
 
 /**
  * `/register` will be route which recieves a `username` and `password` from the front end
  */
 app.post('/register', async (req, res) => {});
+
+/**
+ * `/login` will be a route which recieves a `username` and `password` and checks if it already exists
+ */
+app.post('/login', (req, res) => {});
 
 app.listen(PORT, () => console.log(`App listening on Port: ${PORT}`));
